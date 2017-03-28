7171420a03be7afa1b254126007bc4aaef2ddd18
Mulcam1102 <jhj-jkj@hanmail.net>
2017-03-28 14:32:32 +0900

    마이바티스 값 HashMap으로 받기, sql에 조건절 넣기 ,게시판 페이징하기

---
+++ MyBatis05/src/Test.java
@@ -0,0 +1,49 @@
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Scanner;
+
+import commons.Constant;
+import dao.BoardDao;
+import dao.IBoardDao;
+
+public class Test {
+	public static void main(String[] args) {
+		HashMap<String, Object> params = new HashMap<>();
+		IBoardDao bd = new BoardDao();
+		
+
+//		params.put(Constant.PASS, "1234");
+//		params.put(Constant.NAME, "홍1");
+////		params.put(Constant.EMAIL, "1@naver.com");
+//		params.put(Constant.TITLE, "제목1");
+//		params.put(Constant.CONTENT, "3");
+////		params.put(Constant.READCOUNT, "0");
+//		params.put(Constant.WRITEDATE, new Date());
+//		bd.insertBoard(params);
+//		System.out.println(bd.selectOne(params.get(Constant.NUM)));
+//		params.put("keyword", "목");
+		Scanner scan = new Scanner(System.in);
+		System.out.print("페이지수 입력 ");
+		int page = scan.nextInt();
+		params.put("page", (page-1)*10);
+		
+		int first = (page-1)/10*10+1;
+		int finish = bd.selectAll().size()/10+1;
+		int last = first+9;
+		if(last > finish)
+			last = finish;
+		if(page > finish)
+			System.out.println("없는페이지");
+		else{
+			System.out.println("첫페이지 : 1");
+			System.out.println("시작페이지 : " +first);
+			System.out.println("끝페이지 : " + last);
+			System.out.println("마지막페이지 : "+finish);
+		}
+//		System.out.println(params);
+		
+//		bd.updateBoard(params);
+		for(HashMap<String, Object> h : bd.selectBySearch(params))
+			System.out.println(h);
+	}
+}
---
+++ MyBatis05/src/dao/IBoardDao.java
@@ -0,0 +1,13 @@
+package dao;
+
+import java.util.HashMap;
+import java.util.List;
+
+public interface IBoardDao {
+	public int insertBoard(HashMap<String, Object> params);
+	public int updateBoard(HashMap<String, Object> params);
+	public int deleteBoard(int num);
+	public HashMap<String, Object> selectOne(int num);
+	public List<HashMap<String, Object>> selectAll();
+	public List<HashMap<String, Object>> selectBySearch(HashMap<String, Object> params);
+}
