874d72eb55e2569cd4e9c58209cd9885c911cbc7
student <student@M9017>
2017-04-19 14:32:12 +0900

    json편하게 쓰기

--- Spring_Board_File/src/controller/BoardController.java
+++ Spring_Board_File/src/controller/BoardController.java
@@ -3,8 +3,12 @@ package controller;
 
 
 import java.io.File;
+import java.io.IOException;
 import java.sql.Date;
 import java.text.SimpleDateFormat;
+import java.util.HashMap;
+
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.propertyeditors.CustomDateEditor;
@@ -14,11 +18,16 @@ import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.View;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import model.Board;
+import model.BoardFile;
 import service.IBoardService;
 
 @Controller
@@ -49,9 +58,13 @@ public class BoardController {
 	}
 	//파일다운
 	@RequestMapping("download.do")
-	public View download(int num){
-		File file = new File(service.getFileUri(num));
-		return new DownloadView(file);
+	public View download(int id){
+		//서비스 클래스에 파일 이이디를 주면 
+		//해당 파일의 uri를 갖다 주는 기능흘 추가하고 사용
+//		String uri = service.getFileUri(id);
+		BoardFile boardFile = service.getBoardFile(id);
+		File file = new File(boardFile.getUri());
+		return new DownloadView(file, boardFile.getOriginFileName());
 	}
 	//게시글수정
 	@RequestMapping("update.do")
@@ -80,9 +93,10 @@ public class BoardController {
 	}
 	@RequestMapping("updatePage.do")
 	public ModelAndView updatePage(int num){
-		Board board = service.getBoard(num);
+//		Board board = service.getBoard(num);
 		ModelAndView mav = new ModelAndView();
-		mav.addObject("board", board);
+//		mav.addObject("board", board);
+		mav.addAllObjects(service.getBoard(num));
 		mav.setViewName("updateForm");
 		return mav;
 	}
@@ -111,11 +125,18 @@ public class BoardController {
 	}
 	@RequestMapping("view.do")
 	public ModelAndView selectBoard(int num){
-		Board board = service.readBoard(num);
+//		Board board = service.readBoard(num);
 		ModelAndView mav = new ModelAndView();
-		mav.addObject("board", board);
+//		mav.addObject("board", board);
+		mav.addAllObjects(service.readBoard(num));
 		mav.setViewName("view");
 		return mav;
+	}
+	
+	//Json문자열로 만들어주기 (maven에 jackson DataBind 추가)
+	@RequestMapping("getBoardJson.do")
+	public @ResponseBody Board getBoardJson(HttpServletResponse resp, int num){
+		return (Board)service.getBoard(num).get("board");
 	}
 	
 	//yyyy-MM-dd형식으로 넘어오는 데이타는 Date클래스로 바까줌
