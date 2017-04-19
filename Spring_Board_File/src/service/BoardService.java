cf5f2748ee5f899c4b2b0c108c60ebbef631d246
student <student@M9017>
2017-04-19 14:31:18 +0900

    파일업로드,다운로드, 파일명 UUID로 바꾸기(다운받을땐 원래파일명),

--- Spring_Board_File/src/service/BoardService.java
+++ Spring_Board_File/src/service/BoardService.java
@@ -4,18 +4,23 @@ import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import dao.IBoardDao;
+import dao.IBoardFileDao;
 import model.Board;
+import model.BoardFile;
 
 @Service
 public class BoardService implements IBoardService{
 	@Autowired
 	private IBoardDao dao;
+	@Autowired
+	private IBoardFileDao fdao;
 
 	@Override
 	public void writeBoard(Board board, MultipartFile file) {
@@ -24,10 +29,22 @@ public class BoardService implements IBoardService{
 		File folder = new File(path);
 		if(!folder.exists())
 			folder.mkdirs();
-		String fileName = file.getOriginalFilename();
-		String fileuri = path + fileName;
-		File localFile = new File(fileuri);
 		
+		UUID uuid = UUID.randomUUID();
+		//원본파일명
+		String fileName = file.getOriginalFilename();
+		//파일사이즈
+		int fileSize = (int)file.getSize();
+		//파일경로
+//		String fileuri = path + fileName;
+		String fileuri = path + uuid;
+		
+		BoardFile boardFile = new BoardFile();
+		boardFile.setOriginFileName(fileName);
+		boardFile.setSize(fileSize);
+		boardFile.setUri(fileuri);
+		
+		File localFile = new File(fileuri);
 		try {
 			file.transferTo(localFile);
 		} catch (IllegalStateException e) {
@@ -37,8 +54,13 @@ public class BoardService implements IBoardService{
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		board.setFileuri(fileuri);
+		fdao.insertBoardFile(boardFile);
+		int fileid = boardFile.getId();
+		board.setFileid(fileid);
+		
+//		board.setFileuri(fileuri);
 		dao.insertBoard(board);
+		
 	}
 
 	@Override
@@ -53,6 +75,11 @@ public class BoardService implements IBoardService{
 		String originPass = dao.selectOne(num).getPass();
 		
 		if(pass.equals(originPass)){
+			int f_id = dao.selectOne(num).getFileid();
+			String fileUri = fdao.selectOne(f_id).getUri();
+			File file = new File(fileUri);
+			file.delete();
+			fdao.deleteBoardFile(f_id);
 			dao.deleteBoard(num);
 			return true;
 		}
@@ -61,23 +88,31 @@ public class BoardService implements IBoardService{
 	}
 
 	@Override
-	public Board readBoard(int num) {
+	public HashMap<String, Object> readBoard(int num) {
 		// TODO Auto-generated method stub
 		Board board = dao.selectOne(num);
 		board.setReadcount(board.getReadcount()+1);
 		dao.updateBoard(board);
-		return getBoard(num);
+		BoardFile boardFile = fdao.selectOne(board.getFileid());
+		HashMap<String, Object> params = new HashMap<>();
+		params.put("board", board);
+		params.put("boardFile", boardFile);
+		return params;
 	}
 	
 	@Override
-	public Board getBoard(int num) {
+	public HashMap<String, Object> getBoard(int num) {
 		// TODO Auto-generated method stub
 		Board board = dao.selectOne(num);
-		String uri = board.getFileuri();
-		String[] pathArr = uri.split("/");
-		String filename = pathArr[pathArr.length-1];
-		board.setFileuri(filename);
-		return board;
+		BoardFile boardFile = fdao.selectOne(board.getFileid());
+		HashMap<String, Object> params = new HashMap<>();
+		params.put("board", board);
+		params.put("boardFile", boardFile);
+//		String uri = board.getFileuri();
+//		String[] pathArr = uri.split("/");
+//		String filename = pathArr[pathArr.length-1];
+//		board.setFileuri(filename);
+		return params;
 	}
 
 	@Override
@@ -115,9 +150,9 @@ public class BoardService implements IBoardService{
 	}
 
 	@Override
-	public String getFileUri(int num) {
+	public BoardFile getBoardFile(int id) {
 		// TODO Auto-generated method stub
-		return dao.selectOne(num).getFileuri();
+		return fdao.selectOne(id);
 	}
 
 
